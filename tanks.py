daec9136cb601a61bcb807fcb05fe4732ef3957c
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-15 12:12:23 -0700

    Added substances.

--- tanks.py
+++ tanks.py
@@ -3,24 +3,24 @@ class tank:
 	volume    = float()
 	substance = str()
 
-	def __init__(self, volume): 
+	def __init__(self, volume, substance): 
 
 		# need to specify units
 		self.volume = volume
 
 # gallons
-T1  = tank(10)
-T2  = tank(5)
-T3  = tank(2)
-T4  = tank(2)
-T5  = tank(2)
+T1  = tank(10, None)
+T2  = tank(5, None)
+T3  = tank(2, "H20/Condensate")
+T4  = tank(2, None)
+T5  = tank(2, None)
 
 # liters (why?)
-T6  = tank(2.5)
+T6  = tank(2.5, "pH")
 
 # no units
-T7  = tank()
-T8  = tank()
+T7  = tank(None, "CO2")
+T8  = tank(None, "N2")
 
 # gallons
-T9  = tank(5)
+T9  = tank(5, "Leachate")
