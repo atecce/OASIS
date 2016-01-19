27d640c1fbd5fd12d8fdc77bb4afd601e6099b43
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-18 17:09:15 -0700

    More sensible naming. By individual sensor instead of type.

--- objects/sensors.py
+++ objects/sensors.py
@@ -7,12 +7,14 @@ O = list()
 
 class sensor(threading.Thread):
 
-	function            = str()
+	name	 	    = str()
 	units               = str()
 	sensor_range        = dict()
 	required_resolution = float()
 
-	def __init__ (self):
+	def __init__ (self, name):
+
+		self.name = name
 
 		threading.Thread.__init__(self)
 
@@ -32,8 +34,6 @@ class EC(sensor):
 
 	# Electrical Conductivity (Measuring Nutrient Deficit) in Growth Medium & Reservoir
 
-	name 		    = "EC"
-	
 	# check this again
 	units               = "micro-S-cm-l"	
 
@@ -44,7 +44,6 @@ class pH(sensor):
 
 	# pH in Growth Medium & Reservoir
 
-	name 		    = "pH"
 	units               = "pH"
 	sensor_range        = {"low": 2, "high": 12}
 	required_resolution = .2
@@ -53,7 +52,6 @@ class temperature(sensor):
 
 	# Liquid Temperature in Reservoir (1) & Growth Medium (4)
 
-	name 	            = "temperature"
 	units               = "C"
 	sensor_range        = {"low": 0, "high": 100}
 	required_resolution = 1
@@ -62,7 +60,6 @@ class moisture(sensor):
 
 	# Volumetric Water Content in Growth Medium
 
-	name 		    = "moisture"
 	units               = "%"
 	sensor_range        = {"low": 0, "high": 50}
 	required_resolution = 1
@@ -71,7 +68,6 @@ class DO_probe(sensor):
 
 	# Dissolved Oxygen in Mixing Reservoir
 
-	name 		    = "DO probe"
 	units               = "mg/L"
 	sensor_range        = {"low": 0, "high": 15}
 	required_resolution = .1
@@ -80,7 +76,6 @@ class liquid_level(sensor):
 
 	# Liquid Level in Mixing, Nutrient, pH, Leachate, & Condensate Tanks
 
-	name 		    = "liquid level"
 	units               = "cm"
 	sensor_range        = {"low": 0, "high": 40.5}
 	required_resolution = 1.25
@@ -89,7 +84,6 @@ class flow_meter(sensor):
 
 	# water Flow Rate Into & Out of Growth
 
-	name 		    = "flow meter"
 	units               = "gpm"
 	sensor_range        = {"low": .2, "high": 2}
 	required_resolution = .05
@@ -98,7 +92,6 @@ class RH_temp(sensor):
 
 	# Internal (2) & External (1) Relative Humidity & Air Temperature
 
-	name 		    = "RH temp"
 	units               = "%", "C"
 	sensor_range        = {"low": 5, "high": 99}
 	required_resolution = 1
@@ -107,7 +100,6 @@ class total_pressure(sensor):
 
 	# Internal (1) & External (1) Total Atmospheric Pressure"
 
-	name		    = "total pressure"
 	units               = "kPA"
 	sensor_range        = {"low": 30, "high": 110}
 	required_resolution = 1
@@ -116,7 +108,6 @@ class oxygen(sensor):
 
 	# Internal (1) & External (1) O2 concentration"
 
-	name		    = "oxygen"
 	units               = "%"
 	sensor_range        = {"low": 0, "high": 100}
 	required_resolution = 1
@@ -125,7 +116,6 @@ class CO2(sensor):
 
 	# Internal (1) & External CO2 concentration"
 
-	name		    = "CO2"
 	units               = "ppm"
 	sensor_range        = {"low": 0, "high": 2000}
 	required_resolution = 100
@@ -134,7 +124,6 @@ class light_PAR(sensor):
 
 	# Internal (1) & External (1) Photosynthetically Active Radiation (PAR)"
 
-	name		    = "light PAR"
 	units               = "micro-mol / (m**2 * s)"
 	sensor_range        = {"low": 0, "high": 2000}
 	required_resolution = 2
@@ -143,46 +132,45 @@ class camera(sensor):
 
 	# Plant Health Imagery
 
-	name		    = "camera"
 	units               = "RGB"
 	sensor_range        = {"low": 1, "high": 255}
 	required_resolution = "1k0x1k"
 
-S101 = EC()
-S102 = pH()
-S103 = temperature()
-S104 = EC()
-S105 = liquid_level()
-S106 = liquid_level()
-S107 = liquid_level()
-S108 = liquid_level()
-S109 = liquid_level()
-S110 = flow_meter()
-S111 = flow_meter()
-S112 = liquid_level()
+S101 = EC("S101")
+S102 = pH("S102")
+S103 = temperature("S103")
+S104 = EC("S104")
+S105 = liquid_level("S105")
+S106 = liquid_level("S106")
+S107 = liquid_level("S107")
+S108 = liquid_level("S108")
+S109 = liquid_level("S109")
+S110 = flow_meter("S110")
+S111 = flow_meter("S111")
+S112 = liquid_level("S112")
 
-S201 = temperature()
-S202 = temperature()
-S203 = temperature()
-S204 = temperature()
-S205 = EC()
-S206 = pH()
-S208 = moisture()
-S209 = moisture()
-S210 = moisture()
-S211 = moisture()
+S201 = temperature("S201")
+S202 = temperature("S202")
+S203 = temperature("S203")
+S204 = temperature("S204")
+S205 = EC("S205")
+S206 = pH("S206")
+S208 = moisture("S208")
+S209 = moisture("S209")
+S210 = moisture("S210")
+S211 = moisture("S211")
 
-S301 = RH_temp()
-S302 = RH_temp()
-S303 = total_pressure()
-S304 = oxygen()
-S305 = CO2()
-S306 = light_PAR()
-S307 = camera()
+S301 = RH_temp("S301")
+S302 = RH_temp("S302")
+S303 = total_pressure("S303")
+S304 = oxygen("S304")
+S305 = CO2("S305")
+S306 = light_PAR("S306")
+S307 = camera("S307")
 
-S401 = RH_temp()
-S402 = total_pressure()
-S403 = light_PAR()
+S401 = RH_temp("S401")
+S402 = total_pressure("S402")
+S403 = light_PAR("S403")
 
 sensor_suite = (S101, S102, S103, S104, S105, S106, S107, S108, S109, S110, S111, S112, 
 	        S201, S202, S203, S204, S205, S206,       S208, S209, S210, S211,
