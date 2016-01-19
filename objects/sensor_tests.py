232e03deca1df43bc296e3684030b1d3c94df686
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-18 18:21:08 -0700

    playing with threads and unittest

--- objects/sensor_tests.py
+++ objects/sensor_tests.py
@@ -3,21 +3,26 @@
 # need this for unit tests
 import unittest
 
+# need this for thread tests
+import time
+
 # import the suite of sensors
 from sensors import sensor_suite
 
 class sensor_test(unittest.TestCase):
 
+	# for each sensor, start thread
+	for sensor in sensor_suite: sensor.start()
+
 	def test_ranges(self):
 
 		# sample size
-		n = int(100)
+		n = int()
 
+		# for each sensor
 		for sensor in sensor_suite:
 
-			# start thread
-			sensor.start()
-
+			# initalize sample
 			sample = list()
 
 			# take a sample of size n
@@ -33,5 +38,26 @@ class sensor_test(unittest.TestCase):
 				self.assertTrue(sensor.sensor_range["low"] <= observation <= sensor.sensor_range["high"])
 
 		# more pythonic way with 'for all observation in self.sample: sensor.sensor_range["low"] <= observation <= sensor.sensor_range["high"]'?
+
+	def test_observations(self):
+
+		# initialize length of time (in cycles)
+		T = int(100)
+
+		# initalize list of observations
+		O = list()
+
+		for t in range(T):
+
+			# initalize entry
+			entry = dict()
+
+			# for each sensor, add it's reading
+			for sensor in sensor_suite: entry[sensor.name] = sensor.read()
+
+			print t, entry
+
+			# append entry to list of observations
+			O.append(entry)
 
 if __name__ == "__main__": unittest.main()
