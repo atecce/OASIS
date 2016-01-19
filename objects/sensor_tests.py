073d5a59d007733c41983e889dc3492fcd1d15fb
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-18 17:11:43 -0700

    better comments

--- objects/sensor_tests.py
+++ objects/sensor_tests.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+
+# need this for unit tests
 import unittest
+
+# import the suite of sensors
 from sensors import sensor_suite
 
 class sensor_test(unittest.TestCase):
@@ -15,10 +20,13 @@ class sensor_test(unittest.TestCase):
 			# take a sample of size n
 			for i in range(n): sample.append(sensor.read())
 
+			# for each observation
 			for observation in sample:
 
+				# print the results
 				print sensor.name, sensor.sensor_range, sensor.read()
 
+				# check if observation lies in sensor range
 				self.assertTrue(sensor.sensor_range["low"] <= observation <= sensor.sensor_range["high"])
 
 		# more pythonic way with 'for all observation in self.sample: sensor.sensor_range["low"] <= observation <= sensor.sensor_range["high"]'?
