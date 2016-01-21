96a5306514735d481e119fe903ab5066243f490e
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-21 10:16:25 -0700

    Added connection type. Made sensor calculation much more explicit. Missed DO probe.

--- API/sensors/sensors.py
+++ API/sensors/sensors.py
@@ -1,39 +1,52 @@
 # need this for testing
 import random
 
+# need these to interface with Beaglebone
 import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.PWM  as PWM
 import Adafruit_BBIO.ADC  as ADC
 import Adafruit_BBIO.UART as UART
-
 from Adafruit_I2C import Adafruit_I2C
 
-class sensor():
+class Sensor():
 
 	""" Each sensor has a name, a pin number, a range, and a required resolution. """ 
 
 	name	 	    = str()
-	pin		    = int()
+	pin		    = str()
 	sensor_range        = dict()
+	connection          = str()
 	required_resolution = float()
 
-	def __init__ (self, name, pin):
+	def __init__ (self, name, pin, connection):
 
 		# attributes of a sensor 
-		self.name = name
-		self.pin = pin
+		self.name       = name
+		self.pin        = pin
+		self.connection = connection
 
 		# set up the pin (only takes GPIO now)
-		if pin: GPIO.setup(pin, GPIO.IN)
+		if connection == "GPIO": GPIO.setup(pin, GPIO.IN)
 
 	def read(self): 
 
-		# will read voltage from Beaglebone (for now assumes a random value for testing)
-		if self.pin: voltage = GPIO.input(self.pin)
-		else: voltage = random.uniform(0, 3.3)
+		# will read voltage from Beaglebone (for now assumes a random value for unimplemented sensors)
+		if self.connection == "GPIO": x = GPIO.input(self.pin)
+		else: 			      x = random.uniform(0, 3.3)
 
-		# assumes the function from the voltage is a straight line connecting the two points (y = mx + b)
-		return ((self.sensor_range["high"] - self.sensor_range["low"]) / 3.3) * voltage + self.sensor_range["low"]
+		# assumes the function from the voltage to the units of the sensor 
+		# is a straight line connecting the two points of the range (y = mx + b)
+
+		delta_y = self.sensor_range["high"] - self.sensor_range["low"]
+		delta_x = 3.3
+
+		m = delta_y / delta_x
+
+		b = self.sensor_range["low"]
+
+		y = m*x + b
+
+		return y
 
 class EC(sensor):
 
@@ -113,41 +126,41 @@ class camera(sensor):
 	sensor_range        = {"low": 1, "high": 255}
 	required_resolution = "1k0x1k"
 
-S101 =           EC("S101", None)
-S102 =           pH("S102", None)
-S103 =  temperature("S103", "P8_03")
-S104 =           EC("S104", None)
-S105 = liquid_level("S105", None)
-S106 = liquid_level("S106", None)
-S107 = liquid_level("S107", None)
-S108 = liquid_level("S108", None)
-S109 = liquid_level("S109", None)
-S110 =   flow_meter("S110", None)
-S111 =   flow_meter("S111", None)
-S112 = liquid_level("S112", None)
+S101 =           EC("S101", None,    None)
+S102 =           pH("S102", None,    None)
+S103 =  temperature("S103", "P8_03", "GPIO")
+S104 =           DO("S104", None,    None)
+S105 = liquid_level("S105", None,    None)
+S106 = liquid_level("S106", None,    None)
+S107 = liquid_level("S107", None,    None)
+S108 = liquid_level("S108", None,    None)
+S109 = liquid_level("S109", None,    None)
+S110 =   flow_meter("S110", None,    None)
+S111 =   flow_meter("S111", None,    None)
+S112 = liquid_level("S112", None,    None)
 
-S201 = temperature("S201", "P8_04")
-S202 = temperature("S202", "P8_05")
-S203 = temperature("S203", "P8_06")
-S204 = temperature("S204", "P8_07")
-S205 =          EC("S205", None)
-S206 =          pH("S206", None)
-S208 =    moisture("S208", None)
-S209 =    moisture("S209", None)
-S210 =    moisture("S210", None)
-S211 =    moisture("S211", None)
+S201 = temperature("S201", "P8_04", None)
+S202 = temperature("S202", "P8_05", None)
+S203 = temperature("S203", "P8_06", None)
+S204 = temperature("S204", "P8_07", None)
+S205 =          EC("S205", None,    None)
+S206 =          pH("S206", None,    None)
+S208 =    moisture("S208", None,    None)
+S209 =    moisture("S209", None,    None)
+S210 =    moisture("S210", None,    None)
+S211 =    moisture("S211", None,    None)
 
-S301 =        RH_temp("S301", "P8_08")
-S302 =        RH_temp("S302", "P8_09")
-S303 = total_pressure("S303", None)
-S304 =         oxygen("S304", None)
-S305 =            CO2("S305", None)
-S306 =      light_PAR("S306", None)
-S307 =         camera("S307", None)
+S301 =        RH_temp("S301", "P8_08", None)
+S302 =        RH_temp("S302", "P8_09", None)
+S303 = total_pressure("S303", None,    None)
+S304 =         oxygen("S304", None,    None)
+S305 =            CO2("S305", None,    None)
+S306 =      light_PAR("S306", None,    None)
+S307 =         camera("S307", None,    None)
 
-S401 =        RH_temp("S401", 'P8_10')
-S402 = total_pressure("S402", None)
-S403 =      light_PAR("S403", None)
+S401 =        RH_temp("S401", "P8_10", None)
+S402 = total_pressure("S402", None,    None)
+S403 =      light_PAR("S403", None,    None)
 
 sensor_suite = (S101, S102, S103, S104, S105, S106, S107, S108, S109, S110, S111, S112, 
 	        S201, S202, S203, S204, S205, S206,       S208, S209, S210, S211,
