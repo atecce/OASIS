133c1f6ed45b2f8f07167d6fd95d3847d64dd243
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-22 16:56:56 -0700

    Added HSST's.

---
+++ API/HSST.py
@@ -0,0 +1,40 @@
+class HSST:
+
+	def __init__(self):
+
+		# ppm
+		C02_min = 1000
+		C02_max = 2000
+
+		# kPa
+		pressure_min = 80
+		pressure_max = 84
+
+		# %
+		humidity_min = 50
+		humidity_max = 70
+
+		#celsius
+		day_temp_min = 23
+		day_temp_max = 27
+
+		night_temp_min = 18
+		night_temp_max = 22
+
+		soil_temp_min = 15
+		soil_temp_max = 20
+
+		water_temp_min = 22
+		water_temp_max = 24
+		
+		# micro-Siemens per centimeter
+		EC_min = 1150
+		EC_max = 1250
+
+		# pH
+		pH_min = 5.5
+		pH_max = 6
+
+		# micro-mol per meter-squared seconds
+		PAR_min = 200
+		PAR_max = 250
