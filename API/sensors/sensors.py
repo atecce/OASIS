e92efb1ed4bb9ffcef6974a2e8c2053a9137f342
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-22 16:05:58 -0700

    Added communication type as an attribute.

--- API/sensors/sensors.py
+++ API/sensors/sensors.py
@@ -6,144 +6,156 @@ import Adafruit_BBIO.GPIO as GPIO
 import Adafruit_BBIO.ADC  as ADC
 import Adafruit_BBIO.UART as UART
 from Adafruit_I2C import Adafruit_I2C
-
 import Adafruit_DHT
 
 class sensor():
 
-	""" Each sensor has a name, a pin number, a range, and a required resolution. """ 
+	""" Each sensor has a name, a data communication type, a range, 
+	    a required resolution, and a pin number """ 
 
-	name	 	    = str()
-	pin		    = str()
+	# these are constrained by the sensor type
+	data_comm	    = str()
 	sensor_range        = dict()
 	connection          = str()
 	required_resolution = float()
 
-	def __init__ (self, name, pin, connection):
+	# these are determined at construction
+	def __init__ (self, name, pin):
 
-		# attributes of a sensor 
-		self.name       = name
-		self.pin        = pin
-		self.connection = connection
+		self.name = name
+		self.pin  = pin
 
-	def read(self): 
-
-		# will read voltage from Beaglebone
-		if self.pin: return Adafruit_DHT.read_retry(Adafruit_DHT.DHT22, self.pin) 	
+	# This method depends on quite a bit. Depending on the data communication
+	# type and the sensor, it will use different libraries and return different 
+	# values.
+	def read(self): pass 
 
 class EC(sensor):
 
-	# micro-S-cm-l
+	units		    = " micro-S-cm-l"
+	data_comm	    = "I2C"
 	sensor_range        = {"low": 3, "high": 3000}
 	required_resolution = 1
 
 class pH(sensor): 
 
-	# pH 
+	units		    = "pH"
+	data_comm	    = "I2C"
 	sensor_range        = {"low": 2, "high": 12}
 	required_resolution = .2
 
 class temperature(sensor): 
 
-	# C
+	units		    = "C"
+	data_comm	    = "one-wire"
 	sensor_range        = {"low": 0, "high": 100}
 	required_resolution = 1
 
 class moisture(sensor): 
 
-	# %
+	units		    = "%"
+	data_comm	    = "ADC-I2C"
 	sensor_range        = {"low": 0, "high": 50}
 	required_resolution = 1
 
 class DO_probe(sensor): 
 
-	# mg/L
+	units		    = "mg/L"
+	data_comm	    = "I2C"
 	sensor_range        = {"low": 0, "high": 15}
 	required_resolution = .1
 
 class liquid_level(sensor): 
 
-	# cm
+	units		    = "cm"
+	data_comm	    = "ADC-I2C"
 	sensor_range        = {"low": 0, "high": 40.5}
 	required_resolution = 1.25
 
 class flow_meter(sensor): 
 
-	# gpm
+	units		    = "gpm"
+	data_comm	    = "UART"
 	sensor_range        = {"low": .2, "high": 2}
 	required_resolution = .05
 
 class RH_temp(sensor):
 
-	# C
+	units		    = ("C", "%")
+	data_comm	    = "one-wire"
 	sensor_range        = {"low": 5, "high": 99}
 	required_resolution = 1
 
 class total_pressure(sensor):
 
-	# kPA
+	units		    = "kPA"
+	data_comm	    = "I2C"
 	sensor_range        = {"low": 30, "high": 110}
 	required_resolution = 1
 
 class oxygen(sensor):
 
-	# %
+	units		    = "%"
+	data_comm	    = "ADC-I2C"
 	sensor_range        = {"low": 0, "high": 100}
 	required_resolution = 1
 
 class CO2(sensor):
 
-	# ppm
+	units		    = "ppm"
+	data_comm	    = "UART"
 	sensor_range        = {"low": 0, "high": 2000}
 	required_resolution = 100
 
 class light_PAR(sensor):
 
-	# micro-mol per meters-squared seconds
+	units		    = "micro-mol per meters-squared seconds"
+	data_comm	    = "ADC-I2C"
 	sensor_range        = {"low": 0, "high": 2000}
 	required_resolution = 2
 
 class camera(sensor):
 
-	# RGB
+	units		    = "RGB"
+	data_comm	    = "USB"
 	sensor_range        = {"low": 1, "high": 255}
 	required_resolution = "1k0x1k"
 
-S101 =           EC("S101", None, None)
-S102 =           pH("S102", None, None)
-S103 =  temperature("S103", None, None)
-S104 =     DO_probe("S104", None, None)
-S105 = liquid_level("S105", None, None)
-S106 = liquid_level("S106", None, None)
-S107 = liquid_level("S107", None, None)
-S108 = liquid_level("S108", None, None)
-S109 = liquid_level("S109", None, None)
-S110 =   flow_meter("S110", None, None)
-S111 =   flow_meter("S111", None, None)
-S112 = liquid_level("S112", None, None)
+S101 =           EC("S101", 0x66)
+S102 =           pH("S102", 0x65)
+S103 =  temperature("S103", None)
+S104 =     DO_probe("S104", 0x61)
+S105 = liquid_level("S105", None)
+S106 = liquid_level("S106", None)
+S107 = liquid_level("S107", None)
+S108 = liquid_level("S108", None)
+S109 = liquid_level("S109", None)
+S110 =   flow_meter("S110", None)
+S111 =   flow_meter("S111", None)
+S112 = liquid_level("S112", None)
 
-S201 = temperature("S201", None, None)
-S202 = temperature("S202", None, None)
-S203 = temperature("S203", None, None)
-S204 = temperature("S204", None, None)
-S205 =          EC("S205", None, None)
-S206 =          pH("S206", None, None)
-S208 =    moisture("S208", None, None)
-S209 =    moisture("S209", None, None)
-S210 =    moisture("S210", None, None)
-S211 =    moisture("S211", None, None)
+S201 = temperature("S201", None)
+S202 = temperature("S202", None)
+S203 = temperature("S203", None)
+S204 = temperature("S204", None)
+S205 =          EC("S205", None)
+S206 =          pH("S206", None)
+S208 =    moisture("S208", None)
+S209 =    moisture("S209", None)
+S210 =    moisture("S210", None)
+S211 =    moisture("S211", None)
 
-S301 =        RH_temp("S301", None,   None)
-S302 =        RH_temp("S302", "P8_9", None)
-S303 = total_pressure("S303", None,   None)
-S304 =         oxygen("S304", None,   None)
-S305 =            CO2("S305", None,   None)
-S306 =      light_PAR("S306", None,   None)
-S307 =         camera("S307", None,   None)
+S301 =        RH_temp("S301", None)
+S302 =        RH_temp("S302", "P8_9")
+S303 = total_pressure("S303", 0x77)
+S304 =         oxygen("S304", None)
+S305 =            CO2("S305", None)
+S306 =      light_PAR("S306", None)
+S307 =         camera("S307", None)
 
-S401 =        RH_temp("S401", "P8_10", None)
-S402 = total_pressure("S402", None,    None)
-S403 =      light_PAR("S403", None,    None)
+S401 =        RH_temp("S401", "P8_10")
+S402 = total_pressure("S402", None)
+S403 =      light_PAR("S403", None)
 
 sensor_suite = (S101, S102, S103, S104, S105, S106, S107, S108, S109, S110, S111, S112, 
 	        S201, S202, S203, S204, S205, S206,       S208, S209, S210, S211,
