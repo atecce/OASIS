830f8962e4a64699d3e06023b8023607fa2f4840
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-01-23 01:38:37 -0700

    Added test of standard uniform.

--- API/sensors/teststatistics.py
+++ API/sensors/teststatistics.py
@@ -10,22 +10,42 @@ from statistics import statistics
 
 class statistics_test(unittest.TestCase):
 
+	n = int(10000)
+
 	def test_standard_uniform_distribution(self):
 
 		sample = list()
 
-		n = int(10000)
-
-		for i in range(n): sample.append(random.random())
+		for i in range(self.n): sample.append(random.random())
 
 		standard_uniform = statistics(sample)
 
 		print
-		print "sample mean:", standard_uniform.mu
-		print "expectation:", float(1) / float(2)
+		print "test of standard uniform distribution"
 		print
-		print "sample variance:", standard_uniform.sigma_squared
-		print "variance:", float(1) / float(12)
+		print "\tsample mean:", standard_uniform.mu
+		print "\texpectation:", float(1) / float(2)
+		print
+		print "\tsample variance:", standard_uniform.sigma_squared
+		print "\tvariance:", float(1) / float(12)
+		print
+
+	def test_standard_normal(self):
+
+		sample = list()
+
+		for i in range(self.n): sample.append(random.gauss(0, 1))
+
+		standard_normal = statistics(sample)
+
+		print
+		print "test of standard normal distribution"
+		print
+		print "\tsample mean:", standard_normal.mu
+		print "\texpectation:", 0
+		print
+		print "\tsample variance:", standard_normal.sigma_squared
+		print "\tvariance:", 1
 		print
 
 if __name__ == "__main__": unittest.main()
