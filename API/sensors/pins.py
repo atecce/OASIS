68a8bcf4b9296524d965629180f073753fd2e55c
atecce <atecce@beaglebone.localdomain>
2016-01-24 02:43:22 +0000

    brushed up GPIO input test

--- API/sensors/pins.py
+++ API/sensors/pins.py
@@ -1,15 +1,15 @@
 class pin: pass
 
-class I2C(PIN): 
+class I2C(pin): 
 	
 	ADC = bool()
 
-class UART(PIN): pass
+class UART(pin): pass
 
-class GPIO(PIN):
+class GPIO(pin):
 
 	# GPIO has separate pin
-	def __init__(self, pin):
+	def __init__(self, pin = None):
 
 		self.pin = pin
 
@@ -28,8 +28,8 @@ P8[42]  = GPIO()
 P8[43]  = GPIO()
 P8[44]  = GPIO()
 
-P9[9]   = PIN()
-P9[10]  = PIN()
+P9[9]   = pin()
+P9[10]  = pin()
 
 P9[14]  = GPIO()
 P9[15]  = GPIO()
@@ -38,15 +38,15 @@ P9[29]  = GPIO()
 P9[30]  = GPIO()
 P9[31]  = GPIO()
 
-P9[32]  = PIN()
-P9[33]  = PIN()
-P9[34]  = PIN()
-P9[35]  = PIN()
-P9[36]  = PIN()
-P9[37]  = PIN()
-P9[38]  = PIN()
-P9[39]  = PIN()
-P9[40]  = PIN()
+P9[32]  = pin()
+P9[33]  = pin()
+P9[34]  = pin()
+P9[35]  = pin()
+P9[36]  = pin()
+P9[37]  = pin()
+P9[38]  = pin()
+P9[39]  = pin()
+P9[40]  = pin()
 
 P9[41]  = GPIO()
 P9[42]  = GPIO()
--- API/sensors/test.py
+++ API/sensors/test.py
@@ -1,11 +1,60 @@
 #!/usr/bin/env python
 
+# need this to interact with BBB
+import Adafruit_BBIO.GPIO
+
 # need this for unit tests
 import unittest
 
-# import the suite of sensors
-from sensors import sensor_suite
+# need these for pins
+from pins import P8, P9, GPIO
 
-class sensor_test(unittest.TestCase): pass
+class sensor_test(unittest.TestCase):
+
+	def test_GPIO_in(self):
+
+		# for each pin in P9
+		for number in P8: 
+			
+			# check if it's a GPIO
+			if isinstance(P8[number], GPIO):
+
+				# contruct pin name
+				pin = "P8"+"_"+str(number)
+
+				# display pin
+				print "pin is " + pin 
+
+				# set up pin
+				Adafruit_BBIO.GPIO.setup(pin, Adafruit_BBIO.GPIO.IN)
+
+				# print output of pin
+				if Adafruit_BBIO.GPIO.input(pin): print pin + " is high" 
+				else: print pin + " is low" 
+
+				# clean out pin
+				Adafruit_BBIO.GPIO.cleanup()
+
+		# for each pin in P9
+		for number in P9: 
+			
+			# check if it's a GPIO
+			if isinstance(P9[number], GPIO):
+
+				# contruct pin name
+				pin = "P9"+"_"+str(number)
+
+				# display pin
+				print "pin is " + pin 
+
+				# set up pin
+				Adafruit_BBIO.GPIO.setup(pin, Adafruit_BBIO.GPIO.IN)
+
+				# print output of pin
+				if Adafruit_BBIO.GPIO.input(pin): print pin + " is high" 
+				else: print pin + " is low" 
+
+				# clean out pin
+				Adafruit_BBIO.GPIO.cleanup()
 
 if __name__ == "__main__": unittest.main()
