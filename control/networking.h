49af891967306b04993491c6c5a545a540c2fe6b
Jared Jolton <jared.jolton@gmail.com>
2016-01-27 02:13:26 -0700

    added mysql loading skeleton

--- control/networking.h
+++ control/networking.h
@@ -10,3 +10,31 @@ void dump(float *observation) {
 
 	// some networking required here
 }
+
+// loading data from db (this example should help with writing to db eventually)
+void load() {
+	// interacting with mysql requires the mysql.h header file
+	// do we want to include that file in this file or in the control loop file?
+	
+	MYSQL     *conn;
+	MYSQL_RES *res;
+	MYSQL_ROW *row;
+
+	char *server   = "server";
+	char *user     = "username";
+	char *password = "password";
+	char *database = "local"; // for now
+	
+	conn = mysql_init(NULL);
+	mysql_real_connect(conn, server, user, password, database, 0, NULL, 0);
+	
+	mysql_query(conn, "SELECT * FROM website"); // get all the data
+	
+	res = mysql_use_result(conn);
+	while ((row = mysql_fetch_row(res)) != NULL)
+	{
+		printf(row);
+	}
+	mysql_free_result(res);
+	mysql_close(conn);
+}
