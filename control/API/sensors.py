98aeb0d5b56311eaa12c6433e0aa1e0bd92dc1e7
atecce <atecce@beaglebone.localdomain>
2016-02-02 18:01:15 +0000

    Integrating Sairam's code with OO framework. (these do not work)

--- control/API/sensors.py
+++ control/API/sensors.py
@@ -1,3 +1,6 @@
+import time
+import smbus
+
 class sensor:
 
 	def __init__(self, name, pin):
@@ -11,7 +14,40 @@ class sensor:
 	# values.
 	def read(self): pass
 
-class I2C_sensor(sensor): pass
+class I2C_sensor(sensor): 
+
+	slave_address    = int()
+	interface_number = int()
+
+	def __init__(self, slave_address, interface_number):
+
+		self.slave_address    = slave_address
+		self.interface_number = interface_number
+
+		# create bus with interface number
+		self.bus = smbus.SMBus(interface_number) 
+
+	def read(self):
+
+		time.sleep(3)
+		self.bus.write_byte(self.slave_address, 0x52) #R
+
+		time.sleep(3)
+		results = self.bus.read_i2c_block_data(self.slave_address, 0x52) #read cal info
+
+		time.sleep(3)
+		for index, item in enumerate(results):
+
+			if item == 0:
+				
+				   end_val = index
+				   break
+
+		results = results[1:end_val]
+
+		results_string = ''.join(chr(i) for i in results)
+
+		return results_string
 
 class ADC_sensor(sensor): pass
 
@@ -22,46 +58,49 @@ class one_wire_sensor(sensor): pass
 class USB_sensor(sensor): pass
 
 # liquid tanks and plumbing
-S101 = I2C_sensor("S101", "0x66")	# EC1
-S102 = I2C_sensor("S102", "0x65")	# pH1
-S103 = one_wire_sensor("S103", NULL)	# temp1
-S104 = I2C_sensor("S104", "0x61")	# DO
-S105 = ADC_sensor("S105", NULL)		# LL1
-S106 = ADC_sensor("S106", NULL)		# LL2
-S107 = ADC_sensor("S107", NULL)		# LL3
-S108 = ADC_sensor("S108", NULL)		# LL4
-S109 = ADC_sensor("S109", NULL)		# LL5
-S110 = UART_sensor("S110", NULL)	# flow_met1
-S111 = UART_sensor("S111", NULL)	# flow_met2
-S112 = ADC_sensor("S112", NULL)		# LL6
+S101 = I2C_sensor(0x66, 2)	# EC1
+S102 = I2C_sensor(0x65, 2)	# pH1
+#S103 = one_wire_sensor()	# temp1
+S104 = I2C_sensor(0x61, 2)	# DO
+
+#S105 = ADC_sensor()		# LL1
+#S106 = ADC_sensor()		# LL2
+#S107 = ADC_sensor()		# LL3
+#S108 = ADC_sensor()		# LL4
+#S109 = ADC_sensor()		# LL5
+#S110 = UART_sensor()	# flow_met1
+#S111 = UART_sensor()	# flow_met2
+#S112 = ADC_sensor()		# LL6
 
 # growth medium
-S201 = one_wire_sensor("S201", NULL)	# temp2
-S202 = one_wire_sensor("S202", NULL)	# temp3
-S203 = one_wire_sensor("S203", NULL)	# temp4
-S204 = one_wire_sensor("S204", NULL)	# temp5
-S205 = I2C_sensor("S205", NULL)		# EC2
-S206 = I2C_sensor("S206", NULL)		# pH2
-S208 = ADC_sensor("S208", NULL)		# MO1
-S209 = ADC_sensor("S209", NULL)		# MO2
-S210 = ADC_sensor("S210", NULL)		# MO3
-S211 = ADC_sensor("S211", NULL)		# MO4
+#S201 = one_wire_sensor()	# temp2
+#S202 = one_wire_sensor()	# temp3
+#S203 = one_wire_sensor()	# temp4
+#S204 = one_wire_sensor()	# temp5
+S205 = I2C_sensor(0x64, 2)		# EC2
+S206 = I2C_sensor(0x63, 2)		# pH2
+#S208 = ADC_sensor()		# MO1
+#S209 = ADC_sensor()		# MO2
+#S210 = ADC_sensor()		# MO3
+#S211 = ADC_sensor()		# MO4
 
 # internal atmosphere
-S301 = one_wire_sensor("S301", NULL)	# RHTemp1
-S302 = one_wire_sensor("S302", "P8_9")	# RHTemp2
-S303 = I2C_sensor("S303", "0x77")	# TP1
-S304 = ADC_sensor("S304", NULL)		# O2
-S305 = UART_sensor("S305", NULL)	# C02
-S306 = ADC_sensor("S306", NULL)		# PAR1
-S307 = USB_sensor("S307", NULL)		# camera
+#S301 = one_wire_sensor()	# RHTemp1
+#S302 = one_wire_sensor("P8_9")	# RHTemp2
+#S303 = I2C_sensor()	# TP1
+#S304 = ADC_sensor()		# O2
+#S305 = UART_sensor()	# C02
+#S306 = ADC_sensor()		# PAR1
+#S307 = USB_sensor()		# camera
 
 # external environment
-S401 = one_wire_sensor("S401", "P8_10")	# RHTemp3
-S402 = I2C_sensor("S402", NULL)		# TP2
-S403 = ADC_sensor("S403", NULL)		# PAR2
+#S401 = one_wire_sensor("P8_10")	# RHTemp3
+#S402 = I2C_sensor()		# TP2
+#S403 = ADC_sensor()		# PAR2
 
-sensor_suite = (S101, S102, S103, S104, S105, S106, S107, S108, S109, S110, S111, S112, 
-		S201, S202, S203, S204, S205, S206,       S208, S209, S210, S211,
-		S301, S302, S303, S304, S305, S306, S307,
-		S401, S402, S403)
+print S101.read(), S102.read(), S104.read(), S205.read(), S206.read()
+
+#sensor_suite = (S101, S102, S103, S104, S105, S106, S107, S108, S109, S110, S111, S112, 
+#		S201, S202, S203, S204, S205, S206,       S208, S209, S210, S211,
+#		S301, S302, S303, S304, S305, S306, S307,
+#		S401, S402, S403)
