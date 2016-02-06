76bbb468c4f0dc64cff1be0689635c8b0a6ba179
atecce <atecce@beaglebone.localdomain>
2016-02-06 10:51:12 +0000

    Changed order to match submission document

--- AI/API/sensors/sensors.py
+++ AI/API/sensors/sensors.py
@@ -8,46 +8,46 @@ from USB      import camera
 # needs to be ordered when dumping observation
 sensor_suite = list()
 
-# populate sensor suite
-
-# one camera
-sensor_suite.append(camera)
-
-# six liquid level sensors
-for i in (1, 2, 3, 4, 5, 6): sensor_suite.append(LL[i])
-
-# one dissolved oxygen probe
-sensor_suite.append(DO)
-
-# two pH sensors
-for i in (1, 2): sensor_suite.append(pH[i])
+# populate sensor suite (same order as submission document)
 
 # two electrical conductivity sensors
 for i in (1, 2): sensor_suite.append(EC[i])
 
-# two total pressure sensors
-for i in (1, 2): sensor_suite.append(TP[i])
-
-# two PAR sensors
-for i in (1, 2): sensor_suite.append(PAR[i])
-
-# four moisture sensors
-for i in (1, 2, 3, 4): sensor_suite.append(MO[i])
-
-# one oxygen sensor
-sensor_suite.append(O2)
+# two pH sensors
+for i in (1, 2): sensor_suite.append(pH[i])
 
 # five temperature sensors
 for i in (1, 2, 3, 4, 5): sensor_suite.append(temp[i])
 
-# three relative humidity and temperature sensors
-for i in (1, 2, 3): sensor_suite.append(RHTemp[i])
+# four moisture sensors
+for i in (1, 2, 3, 4): sensor_suite.append(MO[i])
+
+# one dissolved oxygen probe
+sensor_suite.append(DO)
+
+# six liquid level sensors
+for i in (1, 2, 3, 4, 5, 6): sensor_suite.append(LL[i])
 
 # two flow meters
 for i in (1, 2): sensor_suite.append(flow_meter[i])
 
+# three relative humidity and temperature sensors
+for i in (1, 2, 3): sensor_suite.append(RHTemp[i])
+
+# two total pressure sensors
+for i in (1, 2): sensor_suite.append(TP[i])
+
+# one oxygen sensor
+sensor_suite.append(O2)
+
 # one CO2 sensor
 sensor_suite.append(CO2)
+
+# two PAR sensors
+for i in (1, 2): sensor_suite.append(PAR[i])
+
+# one camera
+sensor_suite.append(camera)
 
 for sensor in sensor_suite: 
 
