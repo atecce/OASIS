fd699c668adfd9702e1e981ba834a6a71d0dc6ea
atecce <atecce@beaglebone.localdomain>
2016-02-13 16:14:48 +0000

    Reorganizing sensors by SysID. A mess right now. Will clean up later.

--- AI/API/sensors/sensors.py
+++ AI/API/sensors/sensors.py
@@ -5,10 +5,41 @@ from one_wire import temp, RHTemp
 from UART     import CO2, flow_meter
 from USB      import camera
 
-# needs to be ordered when dumping observation
-sensor_suite = list()
+# populate sensor suite
 
-# populate sensor suite (same order as submission document)
+S = {"101":   I2C_sensor("electrical_conductivity", 0x66),
+     "102":   I2C_sensor("ph_and_circuitry",        0x65),
+     "103":  temperature("liquid_temp", "28-00000673a8a7"),
+     "104":   I2C_sensor("do_probe_and_circuitry",  0x61),
+     "105": liquid_level("liquid_level", 0x80), 
+     "106": liquid_level("liquid_level", 0xA0),
+     "107": liquid_level("liquid_level", 0xC0),
+     "108": liquid_level("liquid_level", 0xE0),
+     "109": liquid_level("liquid_level", 0xD0),
+     "110":   flow_meter("flow_meter_and_circuitry", 1),
+     "111":   flow_meter("flow_meter_and_circuitry", 4),
+     "112": liquid_level("liquid_level", 0xF0),
+
+     "201": temperature("liquid_temp", "28-0000065f27cc"), 
+     "202": temperature("liquid_temp", "28-0000065eb57a"),
+     "203": temperature("liquid_temp", "28-000006747f7f"),
+     "205":  I2C_sensor("electrical_conductivity", 0x64),
+     "206":  I2C_sensor("ph_and_circuitry",	   0x63),
+     "208":    moisture("moisture", 0x80),
+     "209":    moisture("moisture", 0xA0),
+     "210":    moisture("moisture", 0xC0),
+     "211":    moisture("moisture", 0xE0),
+
+     "301":    RH_and_temp("rh_and_air_temp", 'P8_8'),
+     "302":    RH_and_temp("rh_and_air_temp", 'P8_9'),
+     "303": total_pressure("total_pressure", 2),
+     "304": 	    oxygen("O2"),
+     "305":     CO2_sensor("CO2", 5),
+     "306":            PAR("light", 0xF0),
+
+     "401":    RH_and_temp("rh_and_air_temp", 'P8_10'),
+     "402": total_pressure("total_pressure", 1),
+     "403": 	       PAR("light", 0xD0)}
 
 # two electrical conductivity sensors
 for i in (1, 2): sensor_suite.append(EC[i])
