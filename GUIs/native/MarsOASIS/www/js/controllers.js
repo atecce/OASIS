2dea783f0dfe6dc486b5b7173866632cbcb2549c
Prayash Thapa <prayasht@gmail.com>
2016-02-21 05:40:13 -0700

    Create Ionic template.

---
+++ GUIs/native/MarsOASIS/www/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('statsCtrl', function($scope) {
+
+})
+   
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('settingsCtrl', function($scope) {
+
+})
+      
+.controller('loginCtrl', function($scope) {
+
+})
+   
+.controller('registerCtrl', function($scope) {
+
+})
+ 
---
+++ GUIs/native/MarsOASIS/www/js/routes.js
@@ -0,0 +1,65 @@
+angular.module('app.routes', [])
+
+.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
+
+
+
+      .state('tabsController.stats', {
+    url: '/stats',
+    views: {
+      'tab1': {
+        templateUrl: 'templates/stats.html',
+        controller: 'statsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.home', {
+    url: '/home',
+    views: {
+      'tab2': {
+        templateUrl: 'templates/home.html',
+        controller: 'homeCtrl'
+      }
+    }
+  })
+
+  .state('tabsController.settings', {
+    url: '/settings',
+    views: {
+      'tab3': {
+        templateUrl: 'templates/settings.html',
+        controller: 'settingsCtrl'
+      }
+    }
+  })
+
+  .state('tabsController', {
+    url: '/page1',
+    templateUrl: 'templates/tabsController.html',
+    abstract:true
+  })
+
+  .state('login', {
+    url: '/login',
+    templateUrl: 'templates/login.html',
+    controller: 'loginCtrl'
+  })
+
+  .state('register', {
+    url: '/register',
+    templateUrl: 'templates/register.html',
+    controller: 'registerCtrl'
+  })
+
+$urlRouterProvider.otherwise('/page1/stats')
+
+
+
+});
