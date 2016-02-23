75cf906a29145de0dbcfc0d25a437d1f692b038c
Ryan Bohannon <ryanbrohannon@gmail.com>
2016-02-23 16:59:24 -0700

    germinate function

---
+++ control_flow/germinate.py
@@ -0,0 +1,21 @@
+# If level S105 is greater than [HSST_M21]
+
+# Set system mode to germinate
+
+# Activate!
+# Run continuous monitoring and troubleshooting
+# Run Water heating/cooling function
+# Run Nutrient condition function
+# Run pH conditioning function
+
+# activate main pump P1
+if S105 is <= [HSST_M21] 
+
+# deactivate Main pump P1
+
+# set system mode autonomous
+# set time of day
+# set more shit
+# Run time lapse function
+# Run day cycle function
+# run autooperate
