f5ddc2e21189696ed804ad94dcdaf8d2f07ca278
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-02-22 22:07:49 -0700

    Not using django.

--- GUIs/native/django/GUI/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-
-def index(request):
-
-	return HttpResponse('And perhaps, the horizons are limited\n \
-				for those who think the center of the universe,\n \
-				is man.')
