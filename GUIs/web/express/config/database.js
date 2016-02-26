3d50779ee02ea052943125b8646c2f84393ec6b0
Ryan Bohannon <ryanbrohannon@gmail.com>
2016-02-25 19:54:50 -0700

    Added our nodeJS server with express

---
+++ GUIs/web/express/config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+//    'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url' : 'mongodb://rybo:24892489@apollo.modulusmongo.net:27017/Uroro6se' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
