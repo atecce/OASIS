6ba999e4460127d2601d9cf31bf95d194fb9ff9d
Jared Jolton <jared.jolton@gmail.com>
2016-02-25 20:40:05 -0700

    fixed sidebar home links

--- GUIs/web/express/app/routes.js
+++ GUIs/web/express/app/routes.js
@@ -49,6 +49,11 @@ module.exports = function(app, passport) {
     // =====================================
     // we will want this protected so you have to be logged in to visit
     // we will use route middleware to verify this (the isLoggedIn function)
+    app.get('/dashboard', isLoggedIn, function(req, res) {
+        res.render('dashboard.jade', {
+            user : req.user // get the user out of session and pass to template
+        });
+    });
     app.get('/control', isLoggedIn, function(req, res) {
         res.render('control.jade', {
             user : req.user // get the user out of session and pass to template
