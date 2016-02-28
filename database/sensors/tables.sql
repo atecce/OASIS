df7a2f1eaa4fabefbf0dbaaf320d384a974dc069
Alessandro Tecce <alessandro.tecce@colorado.edu>
2016-02-27 20:48:33 -0700

    Massive directory restructure. MAKE SURE YOU PULL THIS FIRST. The files you are probably working on got moved up a directory. Want to change root name from 'OASIS' to 'MarsOASIS.' Will wait to consult with you guys because it may cause problems.

--- database/sensors/tables.sql
+++ database/sensors/tables.sql
@@ -4,14 +4,16 @@ create table if not exists sensors (
 	SenseID     varchar(15)  not null,
 	sensor_type varchar(100) not null,
 	units 	    varchar(100) not null,
+
 	primary key (SysID)
 );
 
 create table if not exists sensor_data (
 
-	sensor_ID int(1)   not null,
+	ID 	  int(1)   not null,
 	read_at   datetime not null default now(),
 	reading   float    not null,
 	primary key (read_at),
-	foreign key (sensor_ID) references sensors (SysID)
+
+	foreign key (ID) references sensors (SysID)
 );
