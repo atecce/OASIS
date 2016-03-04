4b48befd22c2f49f8eba69527fe74fb8bbf2be85
Alessandro Tecce <alte5103@colorado.edu>
2016-03-03 21:57:30 -0700

    Better comments, extraneous lines removed.

--- data_mining/clean.py
+++ data_mining/clean.py
@@ -30,16 +30,13 @@ cleaned = collections.OrderedDict(sorted(cleaned.items()))
 epochs   = cleaned.keys()
 readings = cleaned.values()
 
-print epochs
-print readings
-
 # smooth data
 smoothed_epochs   = np.convolve(epochs,   np.ones((10,))/10, mode = 'valid')
 smoothed_readings = np.convolve(readings, np.ones((10,))/10, mode = 'valid')
 
 smoothed = dict(zip(smoothed_epochs, smoothed_readings))
 
-# make sparse
+# make sparse by only keeping 1 in every 10 entries
 sparse = dict()
 
 count = int()
