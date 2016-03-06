366f3feb349c7f1d5496c7cec2b2b53ce77f85cf
Alessandro Tecce <alte5103@colorado.edu>
2016-03-06 13:52:27 -0700

    Much more concrete analysis on CO2 being sketched out. Writing to csv files to run an exponential regressions and trying to detect gaps in the data to see how long it takes for sensor to fail. This needs work.

--- data_mining/CO2analysis.py
+++ data_mining/CO2analysis.py
@@ -7,19 +7,6 @@ import collections
 # need this to get data
 from REST import REST
 
-# best estimate of ambient parts per million
-PPM_a = 450
-
-# solve for the constant in exponential decay
-def constant(delta_PPM, delta_t):
-
-	return math.log(delta_PPM) / delta_t
-
-# model of parts-per-million over time
-def model(k, t, PPM_a):
-
-	return math.exp(k*t) + PPM_a
-
 # set up backend to get data
 firebase = REST()
 
@@ -35,22 +22,65 @@ CO2data = {int(k): v for k, v in CO2data.items()}
 # need it to be ordered
 CO2data = collections.OrderedDict(sorted(CO2data.items()))
 
+# initialize list of time segments
+segments = list()
+segments.append(list())
+
+segment_index = int()
+
+last = int()
+
+for epoch in CO2data:
+
+	# special case for first entry
+	if last == 0: last = epoch
+
+	# set 30 seconds as threshold for sensor failure
+	elif epoch-last > 30: 
+		
+		# add a new segment
+		segments.append(list())
+		segment_index += 1
+
+		print epoch-last
+
+	# append entry to current segment
+	segments[segment_index].append(epoch)
+
+	# set last entry
+	last = epoch
+
+# initialize list of lengths
+lengths = list()
+
+# initialize list of intervals
+intervals = list()
+
+# for each segment
+for segment in segments:
+
+	# set interval
+	interval = segment[0], segment[-1]
+
+	# append interval to list
+	intervals.append(interval)
+
+	# calculate length of segment
+	length = segment[-1]-segment[0]
+
+	# append length to list
+	lengths.append(length)
+
 # separate keys and values
 epochs   = CO2data.keys()
 readings = CO2data.values()
 
-# initialize list of indices where there are gaps in the readings
-reading_gaps = list()
+# write data to csv
 
-for i in range(1, len(readings)): 
+with open("CO2.csv", 'w') as datafile:
 
-	# set CO2 threshold at 50 parts per-million	
-	if readings[i]-readings[i-1] > .005: 
+	datafile.write("epochs, readings\n")
 
-		print epochs[i-1], epochs[i]
-		print readings[i-1], readings[i]
-
-		# append index 
-		reading_gaps.append(i)
-
-print reading_gaps
+	for k in range(len(epochs)): 
+		
+		datafile.write(str(epochs[k])+', ' + str(readings[k]) + '\n')
