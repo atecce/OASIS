fb52752221a322bbeb65acedb4f8e23eb049d34c
Alessandro Tecce <alte5103@colorado.edu>
2016-03-06 20:23:25 -0700

    Successfully split data up into segments based on readings.

--- data_mining/CO2analysis.py
+++ data_mining/CO2analysis.py
@@ -22,65 +22,40 @@ CO2data = {int(k): v for k, v in CO2data.items()}
 # need it to be ordered
 CO2data = collections.OrderedDict(sorted(CO2data.items()))
 
-# initialize list of time segments
-segments = list()
-segments.append(list())
-
-segment_index = int()
-
-last = int()
-
-for epoch in CO2data:
-
-	# special case for first entry
-	if last == 0: last = epoch
-
-	# set 30 seconds as threshold for sensor failure
-	elif epoch-last > 30: 
-		
-		# add a new segment
-		segments.append(list())
-		segment_index += 1
-
-		print epoch-last
-
-	# append entry to current segment
-	segments[segment_index].append(epoch)
-
-	# set last entry
-	last = epoch
-
-# initialize list of lengths
-lengths = list()
-
-# initialize list of intervals
-intervals = list()
-
-# for each segment
-for segment in segments:
-
-	# set interval
-	interval = segment[0], segment[-1]
-
-	# append interval to list
-	intervals.append(interval)
-
-	# calculate length of segment
-	length = segment[-1]-segment[0]
-
-	# append length to list
-	lengths.append(length)
-
-# separate keys and values
+# split dictionary into two lists
 epochs   = CO2data.keys()
 readings = CO2data.values()
 
-# write data to csv
+# initialize segments with reading gaps
+segments = list()
 
-with open("CO2.csv", 'w') as datafile:
+# first list of segment
+segments.append(list())
 
-	datafile.write("epochs, readings\n")
+# index of segments
+segment_index = int()
 
-	for k in range(len(epochs)): 
-		
-		datafile.write(str(epochs[k])+', ' + str(readings[k]) + '\n')
+# for each reading
+for index in range(1, len(readings)):
+
+	# threshold for gap in readings at 50 ppm
+	if readings[index] - readings[index-1] > .005: 
+
+		# increment segments
+		segments.append(list())
+		segment_index += 1
+
+	# add index of reading to most recent segment
+	segments[segment_index].append(index)
+
+# index csv's starting at 1
+for index in range(1, len(segments)+1):
+
+	# write data to csv
+	with open("CO2_" + str(index)+ ".csv", 'w') as datafile:
+
+		# write headers
+		datafile.write("epochs, readings\n")
+
+		# write each entry
+		for entry in segments[index-1]: datafile.write(str(epochs[entry])+', ' + str(readings[entry])+'\n')
