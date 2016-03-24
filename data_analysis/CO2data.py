d3a5a03ff954456f9b10aff70c1311904f79df25
Alessandro Tecce <alte5103@colorado.edu>
2016-03-23 18:52:00 -0600

    New experimental branch for data analysis.

---
+++ data_analysis/CO2data.py
@@ -0,0 +1,23 @@
+# need this to convert epoch to datetime
+import time
+
+# need this to get data
+from REST import REST
+
+# set up database
+firebase = REST("https://cumarsoasis.firebaseio.com/")
+
+# get data
+CO2data = firebase.GET("sensors/historical/S305.json")
+
+# write csv file
+with open('CO2data.csv', 'w') as f:
+
+	# for each sequential entry
+	for entry in sorted(CO2data):
+
+		# convert epoch to datetime
+		datetime = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(float(entry)))
+
+		# write to csv
+		f.write(str(datetime) + ', ' + str(CO2data[entry]) + '\n')
